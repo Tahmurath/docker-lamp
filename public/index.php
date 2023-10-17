e019bdba0d24274101b087e95f9ee9f0595660e7
Hamidreza <hamidreza.tahmooresi@gmail.com>
2023-10-17 13:41:58 +0330

    move passwords to env + networks

--- public/index.php
+++ public/index.php
@@ -1,3 +1,32 @@
 <?php
+//print_r($_ENV);
 
-phpinfo();
+
+$host = getenv('MYSQL_HOST');
+$dbn = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+//$pass = 234234234;
+
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// mysqli_report(MYSQLI_REPORT_STRICT);
+
+try {
+
+    $conn = new mysqli($host, $user, $pass,$dbn);
+
+} catch (mysqli_sql_exception $e) {
+
+    echo "mysqli_sql_exception : <pre>".print_r($e, true)."</pre><br><hr>";
+
+    die("Unfortunately, the details you entered for connection are incorrect!");
+
+}
+
+
+$listdbtables = array_column($conn->query('SHOW Databases')->fetch_all(),0);
+
+echo "List Databases : <pre>".print_r($listdbtables, true)."</pre><br><hr>";
+
+
+
